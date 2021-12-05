0f5219637f5a8fd65dda4fd2a75f134bf4cea642
bmitrick <bmitrick@gmail.com>
2021-12-05 17:17:26 -0500

    added prims

--- GraphADT/src/graphadt/GraphADT.java
+++ GraphADT/src/graphadt/GraphADT.java
@@ -17,23 +17,53 @@ public class GraphADT {
      * @param args the command line arguments
      */
     public static void main(String[] args){
-        testFloyd();
+        //testFloyd();
+        
+        testPrim();
     }
     
-    private static int[][] makeRandomAdjMatrix(int nodes){
+    private static int[][] makeRandomDirectedAdjMatrix(int nodes){
         int[][] matrix = new int[nodes][nodes];
         
         for(int i = 0; i < nodes; i++){
             for(int j = 0; j < nodes; j++){
-                if (i == j) matrix[i][j] = 0;
+                if (i == j) matrix[i][j] =0;
                 else{
                     //Randomly determines if a node will be connected to another or not. Has a 50/50 chance to be connected
-                
+               
                     //If it is connected, it will be assigned a random weight in the range 1 to 11
                     if(Math.random() < 0.5) matrix[i][j] = (int) (Math.random() * 10)+1;
 
                     //If it is not connected, it will have value 999999 for convenience sake
                     else matrix[i][j] = 999999;
+                }
+                
+            }
+        }
+        
+        return matrix;
+    }
+    
+    private static int[][] makeRandomUndirectedAdjMatrix(int nodes){
+        int[][] matrix = new int[nodes][nodes];
+        
+        for(int i = 0; i < nodes/2 +1 ; i++){
+            for(int j = i; j < nodes; j++){
+                if (i == j) matrix[i][j] = 0;
+                else{
+                    //Randomly determines if a node will be connected to another or not. Has a 50/50 chance to be connected
+               
+                    //If it is connected, it will be assigned a random weight in the range 1 to 11
+                    if(Math.random() < 0.5){
+                        matrix[i][j] = (int) (Math.random() * 10)+1;
+                        matrix[j][i] = matrix[i][j];
+                    }
+
+                    //If it is not connected, it will have value 999999 for convenience sake
+                    else{
+                        matrix[i][j] = 999999;
+                        matrix[j][i] = matrix[i][j];
+                    }
                 }
             }
         }
@@ -55,7 +85,7 @@ public class GraphADT {
 //                            {999999,7, 0, 1},
 //                            {6, 999999, 999999, 0}};
 
-        int[][] matrix1 = makeRandomAdjMatrix(4);
+        int[][] matrix1 = makeRandomDirectedAdjMatrix(4);
         
         System.out.println("Starting Matrix:");
         for(int[] arr: matrix1){
@@ -86,7 +116,7 @@ public class GraphADT {
         
         //Attempting Floyd's on a 10 node matrix
         start = System.currentTimeMillis();
-        int[][] matrix2 = makeRandomAdjMatrix(10);
+        int[][] matrix2 = makeRandomDirectedAdjMatrix(10);
         
         System.out.println("Starting Matrix:");
         for(int[] arr: matrix2){
@@ -117,7 +147,7 @@ public class GraphADT {
         
         //Attempting Floyd's on a 100 node matrix
         start = System.currentTimeMillis();
-        int[][] matrix3 = makeRandomAdjMatrix(100);
+        int[][] matrix3 = makeRandomDirectedAdjMatrix(100);
         
         System.out.println("Starting Matrix:");
         for(int[] arr: matrix3){
@@ -145,7 +175,7 @@ public class GraphADT {
         
         //Attempting Floyd's on a 1000 node matrix
         start = System.currentTimeMillis();
-        int[][] matrix4 = makeRandomAdjMatrix(1000);
+        int[][] matrix4 = makeRandomDirectedAdjMatrix(1000);
         
 //        System.out.println("Starting Matrix:");
 //        for(int[] arr: matrix4){
@@ -173,7 +203,7 @@ public class GraphADT {
         
         //Attempting Floyd's on a 10000 node matrix
         start = System.currentTimeMillis();
-        int[][] matrix5 = makeRandomAdjMatrix(10000);
+        int[][] matrix5 = makeRandomDirectedAdjMatrix(10000);
         
 //        System.out.println("Starting Matrix:");
 //        for(int[] arr: matrix5){
@@ -192,6 +222,48 @@ public class GraphADT {
         end = System.currentTimeMillis();
         
         System.out.println("Constructing and running Floyd's on a graph with 10000 nodes took " + (end-start) + " ms (not including printing graph and output)");
+        System.out.println();
+        System.out.println();
+    }
+    
+    private static void testPrim(){
+        long start;
+        long end;
+        
+        
+        //Prim's testing
+        
+        //Attempting Prim's on a 4 node matrix
+        start = System.currentTimeMillis();
+        
+//        int[][] matrix1  = {{999999,2,1,3},  
+//                            {2,999999,7,999999},
+//                            {1,7,999999,2},
+//                            {3,9999999,2,999999}};
+
+
+        int[][] matrix1 = makeRandomUndirectedAdjMatrix(4);
+        
+        System.out.println("Starting Matrix:");
+        for(int[] arr: matrix1){
+            System.out.println(Arrays.toString(arr));
+        }
+        
+        System.out.println();
+        System.out.println();
+                            
+        WeightedGraph graph1 = new WeightedGraph(matrix1);
+        
+        int[][] finished1 = graph1.prim();
+        
+        System.out.println("Finished Matrix:");
+        for(int[] arr: finished1){
+            System.out.println(Arrays.toString(arr));
+        }
+        
+        end = System.currentTimeMillis();
+        
+        System.out.println("Constructing and running Prim's on a graph with 4 nodes took " + (end-start) + " ms");
         System.out.println();
         System.out.println();
     }
--- GraphADT/src/graphadt/WeightedGraph.java
+++ GraphADT/src/graphadt/WeightedGraph.java
@@ -53,10 +53,58 @@ public class WeightedGraph {
         return workingMatrix;
     }
     
+    /**
+     * Runs Prim's algorithm on a weighted graph
+     * ###GRAPH MUST BE UNDIRECTED###
+     * 
+     * @return minimal spanning tree for the graph
+     */
     public int[][] prim(){
         int[][] mst = new int[adjacencyMatrix.length][adjacencyMatrix.length];
+        for(int[] i: mst){
+            Arrays.fill(i, 999999);
+        }
         
         ArrayList<Integer> visited = new ArrayList<>();
+        
+        //Assume we start at 0
+        visited.add(0);
+        
+        while(visited.size() < adjacencyMatrix.length){
+            int cheapestEdgeWeight = Integer.MAX_VALUE;
+            int cheapestEdgeX = 0;
+            int cheapestEdgeY = 0;
+            
+            //Look through edges attached to already visited nodes
+            for(int i: visited){
+                int x = i;
+                
+                for(int y = 0; y < adjacencyMatrix.length; y++){
+                    //Check you arent looking at the path from one node to itself & that y has not already been visited
+                    if(y != x && !visited.contains(y)){
+                        
+                        //If the edge from node x to node y is the cheapest found so far, update cheapestEdgeWeight and its coords
+                        if(adjacencyMatrix[x][y] < cheapestEdgeWeight){
+                            cheapestEdgeWeight = adjacencyMatrix[x][y];
+                            cheapestEdgeX = x;
+                            cheapestEdgeY = y;
+                        }
+                    }
+                }
+            }
+            
+            visited.add(cheapestEdgeY);
+            mst[cheapestEdgeX][cheapestEdgeY] = cheapestEdgeWeight;
+            mst[cheapestEdgeY][cheapestEdgeX] = cheapestEdgeWeight;
+        }
+        
+        //Manually sets the shortest path from a node to itself as 0
+        for(int i = 0; i < mst.length; i++){
+            for(int j = 0; j < mst.length; j++){
+                if(i == j) mst[i][j] = 0;
+            }
+        }
+        
         
         return mst;
     }
